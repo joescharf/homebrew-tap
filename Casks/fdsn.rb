f39612f355b084b003f2c7136f2da95e5a31331b
releaserbot <joe@joescharf.com>
2026-02-23 12:07:30 -0700

    Brew cask update for fdsn version v0.7.1

--- Casks/fdsn.rb
+++ Casks/fdsn.rb
@@ -3,7 +3,7 @@ cask "fdsn" do
   name "fdsn"
   desc "Seismic station metadata management -- import, explore, and re-serve FDSN data from a single binary."
   homepage "https://github.com/joescharf/fdsn"
-  version "0.7.0"
+  version "0.7.1"
 
   livecheck do
     skip "Auto-generated on release."
@@ -13,7 +13,7 @@ cask "fdsn" do
 
   on_macos do
     url "https://github.com/joescharf/fdsn/releases/download/v#{version}/fdsn_Darwin_universal.zip"
-    sha256 "21dd568516505e161accf188b3046841feddaf8d49597ed7c41c72d1a1a63fa9"
+    sha256 "c9b180bb7d3d59f4c924d0dc1360c66cfc46c18db37011bead6c9d737ec5eab1"
   end
 
   # No zap stanza required
